58af99f0f36280dd0ef29a79b273facc9022bb3a
farhatizakaria <zakariafarhati18@gmail.com>
2025-08-14 00:07:11 +0100

    first git commit

---
+++ CIDR.py
@@ -0,0 +1,12 @@
+cidr_user = int(input('Write your prefered CIDR value: '))
+mask = ['','','','']
+for digit in range(cidr_user+1):
+    if len(mask[0]) < 8:
+        mask[0] += '1'
+    elif len(mask[1]) < 8:
+        mask[1] += '1'
+    elif len(mask[2]) < 8:
+        mask[2] += '1'
+    elif len(mask[3]) < 8:
+        mask[3] += '1'
+print(mask)
