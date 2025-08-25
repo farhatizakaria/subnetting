307ab78a62bf426b09d422a35d63eae1c3f43ea4
farhatizakaria <zakariafarhati18@gmail.com>
2025-08-25 18:57:58 +0100

    Add toBinary() function for converting from decimal to binary

--- converting.py
+++ converting.py
@@ -1,11 +1,30 @@
 # This script made only for converting binary-decimal purposes
 def toDecimal(binary):
-    total = 0
-    k = 7
-    for i in range(0,8):
-        prod = int(binary[i]) * 2 ** k
-        total += prod
-        k = k - 1
-    print(total)
+    if len(binary) == 8:
+        total = 0
+        k = 7
+        for i in range(0,8):
+            prod = int(binary[i]) * 2 ** k
+            total += prod
+            k = k - 1
+        print(total)
+    else:
+        print('Make sure the given binary should be in 8-digits form')
 
-toDecimal('11001111')
+def toBinary(decimal):
+    decimal_result = ''
+    while True:
+        q = decimal // 2
+        r = decimal % 2
+        # We care with the rest of given decimal with 2
+        decimal_result += str(r)
+        # Open the loop to divise the new given result
+        decimal = q
+        if q == 0:
+            break
+    # We need to inverse the result's order
+    result = decimal_result[::-1]
+    print(result)
+
+toDecimal('01100100') # Done
+toBinary(100) # Done
